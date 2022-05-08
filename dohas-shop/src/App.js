796c6d5f4f2fc5039542d98fc70d94466f5d46ba
dohaisalive <dohaisalive@gmail.com>
2022-05-08 19:41:14 +0300

    first task

--- dohas-shop/src/App.js
+++ dohas-shop/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Heart Flower</h1>
+      <p>buy flowers for your loved ones!</p>
+      <img
+        src="https://media.istockphoto.com/photos/flower-shop-picture-id514375117"
+        alt=""
+      ></img>
     </div>
   );
 }
