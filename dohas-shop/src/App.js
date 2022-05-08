4eec26ea5f1653d337b72ae839d9fb521a30ceb3
dohaisalive <dohaisalive@gmail.com>
2022-05-08 20:33:08 +0300

    second task, initial commit

--- dohas-shop/src/App.js
+++ dohas-shop/src/App.js
@@ -1,6 +1,23 @@
 import "./App.css";
+import products from "./products.js";
+
+//how to display data (render), needs to return html, dont play with data here!!!!
+//its a component
+function Product(props) {
+  return (
+    <div className="Product">
+      <p>{props.name}</p>
+      <img src={props.image}></img>
+      <p>{props.price}</p>
+    </div>
+  );
+}
 
 function App() {
+  //make data an array of components so they can be rendered, add them to the Product classname
+  const newproducts = products.map((element) => (
+    <Product name={element.name} image={element.image} price={element.price} />
+  ));
   return (
     <div className="App">
       <h1>Heart Flower</h1>
@@ -9,6 +26,7 @@ function App() {
         src="https://media.istockphoto.com/photos/flower-shop-picture-id514375117"
         alt=""
       ></img>
+      {newproducts}
     </div>
   );
 }
---
+++ dohas-shop/src/products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: "orchid",
+    price: 34,
+    image:
+      "https://www.ikea.com/kw/en/images/products/fejka-artificial-potted-plant-orchid-white__0748880_pe745269_s5.jpg",
+  },
+
+  {
+    name: "jasmine",
+    price: 23,
+    image: "https://gilmour.com/wp-content/uploads/2019/05/Jasmine-Care.jpg",
+  },
+];
+
+export default products;
